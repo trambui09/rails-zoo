133940bb4b2c6ff104966cc95bff591684324caf
Tram Bui <pbui17@cmc.edu>
2020-10-30 15:09:59 -0500

    finished the create routes

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -15,5 +15,34 @@ class AnimalsController < ApplicationController
 
   end
 
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    # without strong params
+    # @animal = Animal.new(
+    #     name: params[:animal][:name],
+    #     species: params[:animal][:species],
+    #     age: params[:animal][:age]
+    # )
+
+    # with strong params
+    @animal = Animal.new(animal_params)
+
+    if @animal.save
+      redirect_to animals_path
+    else
+      render :new, :bad_request
+    end
+
+  end
+
+  private
+
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "animals#index"
   get "/animals", to: "animals#index", as: 'animals'
   get "/animals/new", to: "animals#new", as: "new_animal"
+  post "/animals", to: "animals#create"
 
   # get a specific animal
   get "/animals/:id", to: "animals#show", as: "animal"
