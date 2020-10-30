7b441b6e2bb2c1f66437bf19342daff435f8df14
Tram Bui <pbui17@cmc.edu>
2020-10-30 11:00:23 -0500

    view all animals

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,19 @@
 class AnimalsController < ApplicationController
+
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+    # find animal by their params[:id]
+    @animal = Animal.find_by(id: params[:id])
+
+    if @animal.nil?
+      redirect_to animals_path
+      return
+    end
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "animals#index"
+  get "/animals", to: "animals#index", as: 'animals'
+  get "/animals/new", to: "animals#new", as: "new_animal"
+
+  # get a specific animal
+  get "/animals/:id", to: "animals#show", as: "animal"
+
 end
